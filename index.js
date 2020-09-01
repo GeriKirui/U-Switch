67b48e92fe07d53a45601e2d31acf869bb56853b
Gerald Kirui <geraldkirui8@gmail.com>
2020-09-01 21:42:32 +0200

    Node MCU can now send its latest state.

--- index.js
+++ index.js
@@ -13,28 +13,12 @@ database.loadDatabase();
 //A function that handles a get request from the CLIENT
 //The client is the one getting
 app.get('/api', (request, response) =>{
-
-    let maxIndex = (data) =>{
-        let maxTime = 0;
-        let index = 0;
-        
-        for(let i = 0; i < data.length; i++){
-          if(data[i].timestamp > maxTime){
-            maxTime = data[i].timestamp;
-            index = i;
-          }
-        }
-        return index;
-        }
-
     database.find({}, (err,data) =>{
         if(err){
             response.end();
             return;
         }
         response.json(data);
-        item = data[maxIndex(data)];
-        fs.writeFileSync('public/state.txt', item.state);
     });
 });
 
@@ -42,8 +26,15 @@ app.get('/api', (request, response) =>{
 //The client is the one posting (index.js is the server)
 app.post('/api', (request, response) =>{
     let data = request.body;
+    fs.writeFileSync('public/userState.txt', data.state);
     const timestamp = Date.now();
     data.timestamp = timestamp; 
     database.insert(data);
     response.json(data)
+});
+
+//A function that handles a post request from the node MCU
+app.post('/myLastStatus', (request, response) =>{
+    console.log(request.body.state);
+    fs.writeFileSync('public/switchState.txt', request.body.state);
 });

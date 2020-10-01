b1a14cc68cb817f36f4dba17884e3b684f56db0a
Gerald Kirui <geraldkirui8@gmail.com>
2020-10-01 15:24:24 +0200

    Made major changes to schedule

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const fs = require('fs');
+const wordToNum = require('words-to-numbers')
 const app = express();
 let port = process.env.PORT || 3000;
 app.listen(port, () => console.log('listening at 3000'));
@@ -31,15 +32,39 @@ app.post('/myLastStatus', (request, response) =>{
 
 //A function that gets the scheduling times and stores them on the server
 app.post('/scheduling', (request, response) =>{
-    let schedule = request.body;
-    console.log(schedule.str);
-    fs.writeFileSync('public/schedule.txt', schedule.str);
-    response.json(schedule)
+    let data = request.body;
+    function setCharAt(str,index,chr) {
+        if(index > str.length-1) return str;
+        return str.substring(0,index) + chr + str.substring(index+1);
+    }
+    
+    for(let i = 0; i < data.length; i++){
+        let num1Conv = wordToNum.wordsToNumbers(data[i].num1);
+        let num2Conv = wordToNum.wordsToNumbers(data[i].num2);
+        let index = 4*(num1Conv%12 + 0.25*(num2Conv - 1));
+        let updatedState = JSON.stringify(data[i].state);
+        let schedule = fs.readFileSync('public/schedule.txt', 'utf8');
+        schedule = setCharAt(schedule,index,updatedState);
+        fs.writeFileSync('public/schedule.txt', schedule);
+
+    }
 });
 
-//The client is the one getting
+//A function that sends the schedule to the client
 app.get('/scheduling', (request, response) =>{
-    let x = fs.readFileSync('public/schedule.txt', 'utf8')
-    //console.log(x);
+    let x = fs.readFileSync('public/schedule.txt', 'utf-8');
     response.json(x);
+});
+
+app.get('/command',(request, response) =>{
+    let x = fs.readFileSync('public/userState.txt', 'utf8');
+    response.json(x);
+    let empty = 2;
+    fs.writeFileSync('public/userState.txt', empty);
+})
+
+//A function that handles a post request from the node MCU
+app.post('/myCurrentReadings', (request, response) =>{
+    console.log(request.body);
+    //fs.writeFileSync('public/switchState.txt', request.body.state);
 });
